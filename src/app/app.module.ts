1bcda632685e70b8d26c80556350c17e8cf98fb1
bewithgun <ommore501@gmail.com>
2020-04-26 20:17:30 +0530

    New sub components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,13 @@ import { HelloComponent } from './hello.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipeComponent } from './header/recipe/recipe.component';
 import { ShoppingComponent } from './header/shopping/shopping.component';
+import { RecipeListComponent } from './header/recipe/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './header/recipe/recipe-detail/recipe-detail.component';
+import { ShoppingEditComponent } from './header/shopping/shopping-edit/shopping-edit.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HeaderComponent, RecipeComponent, ShoppingComponent ],
+  declarations: [ AppComponent, HelloComponent, HeaderComponent, RecipeComponent, ShoppingComponent, RecipeListComponent, RecipeDetailComponent, ShoppingEditComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
