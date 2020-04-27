1367a02bff77ac28806065aa7ab875a57a203be2
bewithgun <ommore501@gmail.com>
2020-04-27 12:48:27 +0530

    Add recipeHandler model and use it with dump data.

--- src/app/header/recipe/recipe-list/recipe-list.component.ts
+++ src/app/header/recipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { recipeHandler } from '../recipeHandler.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+recipes: recipeHandler[] = 
+[
+  new recipeHandler('Green Veg Juice','Not tasty lol','https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/articles/health_tools/food_combos_lose_weight_slideshow/493ss_thinkstock_rf_avocado_greens.jpg'),
+  new recipeHandler('Carrot','Noice it is.','https://www.thespruceeats.com/thmb/EqZorXp-y4Fd2jKFmJBrn9Hn9Yw=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/carrots-with-butter-and-seasonings-3052473-hero-01-5c40fb71c9e77c00017175a0.jpg')
+]
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/header/recipe/recipeHandler.model.ts
@@ -0,0 +1,11 @@
+export class recipeHandler 
+{
+  recipeName;
+  recipeDescription;
+  recipeImageUrl;
+  constructor(N,D,I){
+    this.recipeName=N;
+    this.recipeDescription=D;
+    this.recipeImageUrl=I;
+  }
+}
