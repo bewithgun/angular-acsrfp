8422c0ffd889b1757e0f19d6656b6edc5b774a1e
bewithgun <ommore501@gmail.com>
2020-05-03 14:49:25 +0530

    Add new custom directive which enables dropdown and use it.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,10 +11,11 @@ import { RecipeListComponent } from './header/recipe/recipe-list/recipe-list.com
 import { RecipeDetailComponent } from './header/recipe/recipe-detail/recipe-detail.component';
 import { ShoppingEditComponent } from './header/shopping/shopping-edit/shopping-edit.component';
 import { RecipeItemComponent } from './header/recipe/recipe-item/recipe-item.component';
+import { AppDropdownDirective } from './app-dropdown.directive';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HeaderComponent, RecipeComponent, ShoppingComponent, RecipeListComponent, RecipeDetailComponent, ShoppingEditComponent, RecipeItemComponent ],
+  declarations: [ AppComponent, HelloComponent, HeaderComponent, RecipeComponent, ShoppingComponent, RecipeListComponent, RecipeDetailComponent, ShoppingEditComponent, RecipeItemComponent, AppDropdownDirective ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
