276a8d65fbdec643193ea5416a753c0e96faee7e
bewithgun <ommore501@gmail.com>
2020-05-04 20:20:30 +0530

    Replace complex way to get ingredient data of current selected recipe with incomplex way.

--- src/app/header/recipe/recipe-detail/recipe-detail.component.ts
+++ src/app/header/recipe/recipe-detail/recipe-detail.component.ts
@@ -9,18 +9,24 @@ import { ShoppingListServService } from '../../../shared/shopping-list-serv.serv
 })
 export class RecipeDetailComponent implements OnInit {
   @Input() recipeData: recipeHandler;
-  @ViewChild('RecipeIng') RecipeIngJS;
+  // @ViewChild('RecipeIng') RecipeIngJS;
 
   constructor(private ShoppingSV: ShoppingListServService) { }
 
   onAddToShoppingList()
   {
-    for(let i of this.RecipeIngJS.nativeElement.children)
+    // for(let i of this.RecipeIngJS.nativeElement.children)
+    // {
+    //   var IngName = i.children[0].textContent;
+    //   var IngAmount = i.children[1].textContent
+    //   this.ShoppingSV.addIngredient(IngName,IngAmount);
+    // }
+    for(let i of this.recipeData.recipeIngredients)
     {
-      var IngName = i.children[0].textContent;
-      var IngAmount = i.children[1].textContent
-      this.ShoppingSV.addIngredient(IngName,IngAmount);
+    this.ShoppingSV.addIngredient(i.name,i.amount);
+
     }
+    // this.ShoppingSV.addIngredient(recipeData.in);
   }
   ngOnInit(): void {
   }
