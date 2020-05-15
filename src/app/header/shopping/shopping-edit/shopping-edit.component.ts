cc129b3bb48d21a29e2d7abd4c5efe112232eba7
bewithgun <ommore501@gmail.com>
2020-05-15 17:56:53 +0530

    Improve form using template driven approach

--- src/app/header/shopping/shopping-edit/shopping-edit.component.ts
+++ src/app/header/shopping/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter ,ViewChild, ElementRef} from '@angular/core';
 import { ShoppingListServService } from '../../../shared/shopping-list-serv.service';
+import { FormGroup, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -8,9 +9,10 @@ import { ShoppingListServService } from '../../../shared/shopping-list-serv.serv
   
 })
 export class ShoppingEditComponent implements OnInit {
-JFonAddIngredients(nameJV,amountJV)
+
+JFonAddIngredients(form : NgForm)
 {
-  this.shoppingSV.addIngredient(nameJV.value,amountJV.value);
+  this.shoppingSV.addIngredient(form.value.name,form.value.amount);
 }
   constructor(private shoppingSV: ShoppingListServService) { }
 
